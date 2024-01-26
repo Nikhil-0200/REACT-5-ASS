4647f535f171434d31d67646d7fa009ed6d2366c
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-01-26 15:45:15 +0530

    Assingment Completed

--- 260124_making_network_request_react/Making-Network-Request-In-React/src/App.jsx
+++ 260124_making_network_request_react/Making-Network-Request-In-React/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
 import DataDisplay from './Components/DataDisplay'
 
 function App() {
@@ -7,11 +6,36 @@ function App() {
   const [isLoading, setisLoading] = useState(false);
   const [error, setError] = useState(null); 
 
-  
+
+
+
+
+  const API_URL = "https://jsonplaceholder.typicode.com/posts"
+
+  async function fetchData(){
+    try {
+      setisLoading(true)
+      const res = await fetch(API_URL)
+      const finalData = await res.json()
+      setData(finalData)
+    } catch (error) {
+      setError("No Data Found")
+    }
+    finally{
+      setisLoading(false)
+    }
+  }
 
   return (
     <>
-    <p>Hi</p>
+    <p>Making Network Request On Click With React</p>
+    <button onClick={()=>fetchData()}>FetchData</button>
+    <DataDisplay
+    Load={isLoading}
+    Error = {error}
+    Array={data}
+    />
+    
     </>
   )
 }
--- 260124_making_network_request_react/Making-Network-Request-In-React/src/Components/DataDisplay.jsx
+++ 260124_making_network_request_react/Making-Network-Request-In-React/src/Components/DataDisplay.jsx
@@ -1,5 +1,18 @@
-function DataDisplay(){
-
+function DataDisplay(data){
+return(
+    <>
+    <p>{data.Load == false ? "Data Not Loaded" : "Data Loaded"}</p>
+    <p>{data.Error}</p>
+    <div>
+        {data.Array.map((ele)=>(
+            <div key={ele.id} style={{border: "2px solid black", margin: "20px"}}>
+                <p>User Id: {ele.id}</p>
+            <p>Title: {ele.title}</p>
+            </div>
+        ))}
+    </div>
+    </>
+)
 }
 
 export default DataDisplay;
